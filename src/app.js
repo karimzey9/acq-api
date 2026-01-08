ee5936669db3f40eafea4f9fe1d72452a2dd1c3d
karimze <updykarim99@gmail.com>
2026-01-08 13:28:43 -0600

    feat: initialize acq-api project with Express and dotenv
    - Add package.json with dependencies for express and dotenv
    - Create app.js to set up the Express application and define a basic route
    - Create index.js as the entry point for the application
    - Create server.js to run the Express server and implement logging

---
+++ src/app.js
@@ -0,0 +1,12 @@
+// set up express application with middleware and routes
+
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello, from acq-api!');
+});
+
+
+export default app;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+// starting point 
+
+import 'dotenv/config';
+import './server.js';
+
